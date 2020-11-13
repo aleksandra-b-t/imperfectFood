ebb0da27228e448fa612cc9d317db47fcfba9c5e
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2020-11-12 19:31:14 -0500

    general code cleaning

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, {useRef} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import GoToMyAccount from './GoToMyAccount.js';
-import Carusel from './Carusel.js'
+import Carousel from './Carousel.js'
 import Test from './Test.js'
 
 
@@ -12,8 +12,8 @@ export default function App() {
     <View style={styles.container}>
       
       <GoToMyAccount />
-      {/* <Carusel /> */}
-      <Test/>
+      <Carousel />
+      {/* <Test/> */}
     </View>
   );
 }
